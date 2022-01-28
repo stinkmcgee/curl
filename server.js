d1ee8383a4c92a8b5caefc00b208a45c5de7bdbf
stinkmcgee <91292007+stinkmcgee@users.noreply.github.com>
2022-01-28 13:28:20 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res)=>{
+    res.send("Hello browser");
+});
+
+app.post('/login',(req,res)=>{
+    console.log(JSON.stringify(req.body));
+    if(req.body.userName == "maddiec" && req.body.password=="gross"){
+        res.send("Welcome")
+    } else{
+        res.send("Whomst is you? ");
+    }
+});
+
+app.listen(port,()=>{});
